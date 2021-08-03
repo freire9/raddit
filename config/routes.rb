d38a5d3072bb7856544cbf8924e597fdee2e8486
freire9 <daniel.freire@ug.uchile.cl>
2021-08-03 18:14:58 -0400

    ♻️ Deleted some unused routes of comments. Now only have the routes for destroy and create. Changed "Like_link_path" for "like_link_path"

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :comments
+  resources :comments, only: :destroy
   devise_for :users
- # devise_for :installs
+  #devise_for :installs
   root to: "links#index"
   resources :links do
     member do
-      put "Like", to: "links#upvote"
+      put "like", to: "links#upvote"
       put "dislike", to:"links#downvote"
     end
-    resources :comments
+    resources :comments, only: :create
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
